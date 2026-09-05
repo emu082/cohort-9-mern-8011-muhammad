da7aff9aa55884845cfad7149936422a633de58e
M Emaad Haroon <emaadharoon@gmail.com>
2026-09-06 04:49:31 +0500

    add axios client and notes api helper

---
+++ frontend/src/api/notesApi.js
@@ -0,0 +1,12 @@
+import axiosClient from "./axiosClient";
+
+export const fetchNotes = (search = "") =>
+  axiosClient.get("/notes", { params: search ? { search } : {} }).then((r) => r.data.notes);
+
+export const fetchNote = (id) => axiosClient.get(`/notes/${id}`).then((r) => r.data.note);
+
+export const createNote = (data) => axiosClient.post("/notes", data).then((r) => r.data.note);
+
+export const updateNote = (id, data) => axiosClient.put(`/notes/${id}`, data).then((r) => r.data.note);
+
+export const deleteNote = (id) => axiosClient.delete(`/notes/${id}`);
