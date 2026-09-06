96102b5112b13eddbb21116bb8c7fff10b28de7a
M Emaad Haroon <emaadharoon@gmail.com>
2026-09-06 13:26:34 +0500

    add real-time note updates with socket.io

--- backend/src/controllers/noteController.js
+++ backend/src/controllers/noteController.js
@@ -1,6 +1,11 @@
 const catchAsync = require("../utils/catchAsync");
 const noteService = require("../services/noteService");
 
+function notifyUser(req, event, data) {
+  const io = req.app.get("io");
+  if (io) io.to(`user-${req.user.id}`).emit(event, data);
+}
+
 const list = catchAsync(async (req, res) => {
   const notes = await noteService.listNotes(req.user.id, req.query.search);
   res.json({ success: true, notes });
@@ -13,16 +18,19 @@ const getOne = catchAsync(async (req, res) => {
 
 const create = catchAsync(async (req, res) => {
   const note = await noteService.createNote(req.user.id, req.body);
+  notifyUser(req, "note:created", note);
   res.status(201).json({ success: true, note });
 });
 
 const update = catchAsync(async (req, res) => {
   const note = await noteService.updateNote(req.user.id, req.params.id, req.body);
+  notifyUser(req, "note:updated", note);
   res.json({ success: true, note });
 });
 
 const remove = catchAsync(async (req, res) => {
   await noteService.deleteNote(req.user.id, req.params.id);
+  notifyUser(req, "note:deleted", { id: Number(req.params.id) });
   res.status(204).send();
 });
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,45 @@
 require("dotenv").config();
+const http = require("http");
+const { Server } = require("socket.io");
+const jwt = require("jsonwebtoken");
+
 const app = require("./app");
 const { connectDB } = require("./config/db");
 const { syncModels } = require("./models");
 const logger = require("./config/logger");
 
 const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+io.use((socket, next) => {
+  const token = socket.handshake.auth?.token;
+  if (!token) return next(new Error("No token provided"));
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    socket.userId = payload.userId;
+    next();
+  } catch (err) {
+    next(new Error("Invalid token"));
+  }
+});
+
+io.on("connection", (socket) => {
+  
+  socket.join(`user-${socket.userId}`);
+});
+
+
+app.set("io", io);
 
 async function start() {
   await connectDB();
   await syncModels();
-  app.listen(PORT, () => {
+  server.listen(PORT, () => {
     logger.info(`Server running on port ${PORT}`);
   });
 }
